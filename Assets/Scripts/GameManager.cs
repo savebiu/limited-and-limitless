d1af65e6f16d6614c0781df588926cd690f2b384
save_abc <2424824316@qq.com>
2024-07-07 15:25:23 +0800

    final

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -71,7 +71,7 @@ public class GameManager : MonoBehaviour
     }
     public void QuitGame()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        
         Application.Quit();
     }
 }
--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Security.Cryptography;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.UI;
 
